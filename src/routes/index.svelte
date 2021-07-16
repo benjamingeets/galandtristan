90e62f5296e322317c12d38047bb3434d326760a
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 16:58:04 +0200

    css fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,11 +9,13 @@ onMount(()=>{page.update(n => "")})//Update the store value to change CSS in nav
     <title>Galand Tristan // Web design, Infographie,... // Tournai (Belgique)</title>
 </svelte:head>
 
-<main class="bg-turquoise text-white flex px-60">
-    <section class="flex flex-col items-start justify-center w-6/12">
-        <h2 class="text-7xl">Galand Tristan</h2>
-        <p class="mr-20 text-2xl">Je m’appelle Galand Tristan. Je suis étudiant en e-business et passionné par le graphisme et le Web design. </p>
-        <button>Mon travail</button>
-    </section>
-    <Tristan style="relative -bottom-36"/>
-</main>
+<div class="bg-turquoise">
+    <main class="text-white flex max-w-screen-xl mx-auto flex-wrap pt-20">
+        <section class="flex flex-col items-start justify-center w-5/12">
+            <h2 class="text-7xl custom-font">Galand Tristan</h2>
+            <p class="mr-20 text-2xl">Je m’appelle Galand Tristan. Je suis étudiant en e-business et passionné par le graphisme et le Web design. </p>
+            <button>Mon travail</button>
+        </section>
+        <Tristan style="relative -bottom-28"/>
+    </main>
+</div>
