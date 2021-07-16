b0cbb6c14764e084f7038ccff5bc6684d370f994
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 17:28:00 +0200

    edit footer text

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,6 @@
 <script>
+import { getStores } from "$app/stores";
+
 import Social from "./Social.svelte"
 const socials = [
     {icon:"linkedin",url:"https://linkedin.com/in/galandtristan"},
@@ -6,13 +8,14 @@ const socials = [
     {icon:"insta",url:"https://instagram.com/galandtristan.graphisme"},
     {icon:"mail", url:"mailto:galandtristan@gmail.com"}
 ]
+const year = new Date()
 </script>
 
-<footer class="bg-turquoise absolute bottom-0 w-full h-36 flex justify-center flex-col items-center">
+<footer class="bg-turquoise mt-10 w-full h-36 flex justify-center flex-col items-center">
     <div>
         <ul>
             <Social/>
         </ul>
     </div>
-    <p class="text-white">Développé par <a target="_blank" class="border-b-2" href="https://geets.dev">Benjamin Geets</a></p>
+    <p class="text-white">galandtristan.be © {year.getFullYear()} // <a href="https://geets.dev">{"<geets.dev/>"}</a></p>
 </footer>
