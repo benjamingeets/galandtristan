3d91798703b247ea31ee6cbbc1188d8ace7c13f4
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 21:24:13 +0200

    sticky footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,12 @@
-<script>import "../app.postcss";
+<script>
+import Footer from "../components/Footer.svelte";
+import "../app.postcss";
 import Navbar from "../components/Navbar.svelte"
 </script>
-<Navbar/>
+<div class="flex flex-col justify-between min-h-screen">
+    <div>
+        <Navbar/>
 <slot></slot>
+    </div>
+<Footer/>
+</div>
