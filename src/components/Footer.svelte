6018bd90ce631c90f1f951e78115d8d5cdfcdc9c
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 16:57:36 +0200

    added icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,5 @@
 <script>
+import Social from "./Social.svelte"
 const socials = [
     {icon:"linkedin",url:"https://linkedin.com/in/galandtristan"},
     {icon:"youtube",url:"https://youtube.com"},
@@ -10,9 +11,7 @@ const socials = [
 <footer class="bg-turquoise absolute bottom-0 w-full h-36 flex justify-center flex-col items-center">
     <div>
         <ul>
-            {#each socials as link}
-                {link.url}
-            {/each}
+            <Social/>
         </ul>
     </div>
     <p class="text-white">Développé par <a target="_blank" class="border-b-2" href="https://geets.dev">Benjamin Geets</a></p>
