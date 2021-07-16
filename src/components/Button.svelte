cd06f92a1c3f7e0bafdcb7b732938d0e2d39e7f3
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 21:23:45 +0200

    edit text size

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,10 +6,10 @@
 
 {#if target == "_blank"}
 <a target="_blank" href="{href}" class="border-2 border-{color} rounded-md">
-    <p class="mx-4 my-2 text-{color}"><slot></slot></p>
+    <p class="mx-4 my-2 text-lg text-{color}"><slot></slot></p>
 </a>
 {:else}
 <a href="{href}" class="border-2 border-{color} rounded-md">
-    <p class="mx-4 my-2 text-{color}"><slot></slot></p>
+    <p class="mx-4 my-2 text-lg text-{color}"><slot></slot></p>
 </a>
 {/if}
