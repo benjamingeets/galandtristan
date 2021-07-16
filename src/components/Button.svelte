3c5d907bebe5e0c78614ea8ea84f6ed969b92ae3
benjamingeets <benjamingeets@tutamail.com>
2021-07-16 18:05:23 +0200

    new button

---
+++ src/components/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let color = "turquoise"
+    export let href = "#"
+    export let target = "_self"
+</script>
+
+{#if target == "_blank"}
+<a target="_blank" href="{href}" class="border-2 border-{color} rounded-md">
+    <p class="mx-4 my-2 text-{color}"><slot></slot></p>
+</a>
+{:else}
+<a href="{href}" class="border-2 border-{color} rounded-md">
+    <p class="mx-4 my-2 text-{color}"><slot></slot></p>
+</a>
+{/if}
