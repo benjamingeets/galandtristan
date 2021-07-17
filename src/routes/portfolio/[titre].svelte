c91cc4ae841ab4d98ca00a95f2d9f1ffb3eb9eda
benjamingeets <benjamingeets@tutamail.com>
2021-07-17 11:12:04 +0200

    create page

---
+++ src/routes/portfolio/[titre].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+export async function load(ctx) {
+  let slug = ctx.page.params.titre
+  return { props: { slug }}
+}
+</script>
+
+<script>
+       import { onMount } from "svelte";
+    import { page } from '../../store';
+        onMount(()=>{
+            page.update(n => "portfolio")
+        })
+        export let slug
+</script>
+
+<div>{slug}</div>
