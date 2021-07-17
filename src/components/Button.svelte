0b90df9a859ef0313962cbbbc4c969b643bf12b3
benjamingeets <benjamingeets@tutamail.com>
2021-07-17 09:10:11 +0200

    set button style

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,15 +1,15 @@
 <script>
-    export let color = "turquoise"
+    export let style =""
     export let href = "#"
-    export let target = "_self"
+    export let target = ""
 </script>
 
-{#if target == "_blank"}
-<a target="_blank" href="{href}" class="border-2 border-{color} rounded-md">
-    <p class="mx-4 my-2 text-lg text-{color}"><slot></slot></p>
+{#if style == "outline"}
+<a target={target} href="{href}" class="border-2 border-turquoise rounded-md">
+    <p class="mx-4 my-2 text-lg text-turquoise"><slot></slot></p>
 </a>
 {:else}
-<a href="{href}" class="border-2 border-{color} rounded-md">
-    <p class="mx-4 my-2 text-lg text-{color}"><slot></slot></p>
+<a target={target} href="{href}" class="border-2 bg-white border-turquoise rounded-md">
+    <p class="mx-4 my-2 text-lg text-turquoise"><slot></slot></p>
 </a>
 {/if}
