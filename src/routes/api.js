db671aef7c791ac18bccf3125aa1e5667a3ea6f6
benjamingeets <benjamingeets@tutamail.com>
2021-07-17 11:03:49 +0200

    using good old api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,17 +1,11 @@
-
-export const TOKEN = import.meta.env.VITE_TOKEN;
 export const get = async () =>{
-    const res = await fetch('https://api.notion.com/v1/databases/6bc64d27-866b-45fe-a00f-6bac586f0e30', { 
-        method: 'get', 
-        headers: new Headers({
-          'Authorization': `${TOKEN}`, 
-          'Notion-Version': '2021-05-13'
-        })
+    const res = await fetch('https://api.galandtristan.be/projets?_sort=date:DESC', { 
+        method: 'get'
       });
-    const data = await res.json()
+    const portfolio = await res.json()
     return{
         body:{
-            data
+            portfolio
         }
     }
 }
