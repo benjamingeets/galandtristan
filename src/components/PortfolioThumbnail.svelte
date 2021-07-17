0c843abb94fe7fb39bdd6b14d19dbf402f6af584
benjamingeets <benjamingeets@tutamail.com>
2021-07-17 11:03:43 +0200

    add portfolio thumbnail

---
+++ src/components/PortfolioThumbnail.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let titre = "err"
+    export let image = "#"
+    export let link = "#"
+</script>
+
+<div class="lg:w-3/12 md:w-6/12 w-12/12 p-4">
+    <a href="{link}">
+        <img src="{image}" alt="{titre}" title="{titre}">
+    </a>
+</div>
--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -13,16 +13,22 @@ export const load = async ({fetch}) =>{
 </script>
 
 <script>
+    import PortfolioThumbnail from "../../components/PortfolioThumbnail.svelte";
     import { onMount } from "svelte";
     import { page } from '../../store';
         onMount(()=>{
             page.update(n => "portfolio")
         })
     export let data
+    const projets = data.portfolio
     </script>
 
 <svelte:head>
     <title>Portfolio // Galand Tristan</title>
 </svelte:head>
 
-{JSON.stringify(data)}
+<div class="flex flex-wrap max-w-screen-xl mx-auto mt-2">
+    {#each projets as projet}
+     <PortfolioThumbnail titre="{projet.titre}" link="/portfolio/{projet.slug}" image="https://api.galandtristan.be{projet.image.url}"/>
+    {/each}
+</div>
