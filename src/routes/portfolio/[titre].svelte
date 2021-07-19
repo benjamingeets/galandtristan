79e26e66a4866cf509bc635e7e793bc8c0ceb178
benjamingeets <benjamingeets@tutamail.com>
2021-07-19 18:04:32 +0200

    markdown and figma

--- src/routes/portfolio/[titre].svelte
+++ src/routes/portfolio/[titre].svelte
@@ -1,4 +1,9 @@
 <script context="module">
+
+  import marked from "marked"
+  marked.setOptions({
+    baseUrl:"https://api.galandtristan.be"
+  })
   export const load = async (context) => {
     const slug = context.page.params.titre
     const projet = await fetch(`https://api.galandtristan.be/projets?slug=${slug}`)
@@ -13,20 +18,38 @@
   </script>
 
 <script>
+import Button from "../../components/Button.svelte";
 import { onMount } from "svelte";
 import { page } from '../../store';
 export let res
 const projet = res[0]
+const titre = projet.titre
 onMount(()=>{
   page.update(n => "portfolio") 
 
 })
-
-  
-
+const description = marked(res[0].description);
 </script>
 
-<article class="max-w-screen-xl mx-auto mt-2">
-  <img src="https://api.galandtristan.be{projet.image.url}" alt="">
-  <h2 class="custom-font text-4xl">{projet.titre}</h2>
+<svelte:head>
+    <title>{titre} // Galand Tristan</title>
+</svelte:head>
+
+<article class="max-w-screen-md mx-auto mt-2 px-4">
+  {#if projet.figma != null}
+  <iframe
+  height="450"
+  width="800"
+  src="https://www.figma.com/embed?embed_host=astra&url={projet.figma}"
+  allowfullscreen
+/>
+  {/if}
+    {@html description}
+
+  <div class="flex mt-4">
+    {#if projet.bouton != null}
+    <Button target="_blank" href="{projet.bouton.lien}" >{projet.bouton.texte}</Button>
+  {/if}
+  </div>
 </article>
+
