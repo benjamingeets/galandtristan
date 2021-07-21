b01c0fb300d1deaa4aa57771fd85dd6a815bf5c5
benjamingeets <benjamingeets@tutamail.com>
2021-07-21 14:38:37 +0200

    removed lines

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -7,33 +7,17 @@
 
 <svelte:head>
     <title>{title}</title>
-    <meta
-    name="description"
-    content={description}
-  />
+    <meta name="description" content={description}/>
   <link rel="canonical" href="https://galandtristan.be/{lien}">
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://galandtristan.be/" />
   <meta property="og:title" content={title} />
-  <meta
-    property="og:description"
-    content={description}
-  />
+  <meta property="og:description" content={description}/>
   <meta name="author" content="Galand Tristan">
-  <meta
-    name="image"
-    property="og:image"
-    content={image}
-  />
+  <meta name="image" property="og:image" content={image} />
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://galandtristan.be/" />
   <meta property="twitter:title" content={title} />
-  <meta
-    property="twitter:description"
-    content={description}
-  />
-  <meta
-    property="twitter:image"
-    content={image}
-  />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={image} />
 </svelte:head>
