d7ae3fd5c776716b2d9d0e509e20dd7770949cbe
benjamingeets <benjamingeets@tutamail.com>
2021-07-22 15:13:26 +0200

    create 404

---
+++ src/routes/__error.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Button from '../components/global/Button.svelte';
+import Head from '../components/Head.svelte'
+</script>
+
+<Head/>
+
+<div class="text-center mt-10">
+    <h1 class="custom-font text-4xl">Page introuvable</h1>
+    <div class="flex justify-center mt-10">
+        <Button href="/">Retour à l'accueil</Button>
+    </div>
+</div>
