41e9ebabec3215239a6e552bf866972c281f9650
benjamingeets <benjamingeets@tutamail.com>
2021-07-24 14:40:11 +0200

    tiens tiens tiens

--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -2,7 +2,6 @@
     export const load = async ({fetch}) =>{
     const res = await fetch ("/api")
     const data = await res.json()
-
         return {
             props:{
                 data
@@ -25,62 +24,25 @@
 
     const projets = data.portfolio
     const categories = data.categories
-    let selector =""
-    let projetsToDisplay = projets
-    let currentCategorie = "Web design"
-    /*const setProjets = (cat)=>{
-        projetsToDisplay = []
-        currentCategorie = cat
-        if(cat == "tout"){
-            projetsToDisplay = projets
-        }else{
-            projets.forEach((projet) =>{
-                projet.categories.forEach((categorie) =>{
-                    if(categorie.nom == cat){
-                        projetsToDisplay.push(projet)
-                    }
-            })
-        })
-        }
-    }*/
+
     </script>
 
 <Head title="Portfolio // Galand Tristan" lien="portfolio"/>
 
-<!-- Sélection desktop -->
+
 <div class="max-w-screen-xl hidden md:block mx-auto my-16">
     <p class="text-center">
-      <!--  {#if currentCategorie == "tout"}
-         <span class="mx-4 cursor-pointer cat-selected" on:click={()=>{setProjets("tout")}}>Tout</span>
-        {:else}
-          <span class="mx-4 cursor-pointer" on:click={()=>{setProjets("tout")}}>Tout</span>
-        {/if} -->
 
         {#each categories as categorie}
-            {#if currentCategorie == categorie.nom}
                <span class="mx-4 cursor-pointer cat-selected">{categorie.nom}</span>
-            {:else}
-             <span class="mx-4 cursor-pointer">{categorie.nom}</span>
-            {/if}
+
         {/each}
 
         <span class="mx-4 cursoir-pointer"><a target="_blank" href="https://instagram.com/galandtristan.graphisme">Graphisme <ExternalIcon/> </a></span>
     </p>
 </div>
 
-<!-- Sélection mobile 
 
-<div class="mx-4 md:hidden my-20">
-    
-    <select bind:value={selector} on:change={setProjets(selector)} class="w-full border-2 border-turquoise rounded-md mx-auto block h-16" name="" id="">
-        <option value="tout">Tout</option>
-        {#each categories as categorie}
-            <option value="{categorie.nom}">{categorie.nom}</option>
-        {/each}
-    </select>
-</div>
--->
-<!-- Affichage -->
 
 <div class="flex flex-wrap max-w-screen-xl mx-auto mt-2">
     {#each projets as projet}
