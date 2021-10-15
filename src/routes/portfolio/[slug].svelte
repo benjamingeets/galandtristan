2f05f81d93d69898fc8235fcc672a88bf66e6fc1
Benjamin Geets <benjamingeets@tutamail.com>
2021-10-15 18:12:30 +0200

    Update [slug].svelte

--- src/routes/portfolio/[slug].svelte
+++ src/routes/portfolio/[slug].svelte
@@ -43,8 +43,14 @@ allowfullscreen
 <article class="markdown text-white text-lg">
     {@html displayMd(projet.contenu)}
 </article>
+<div class="flex justify-center gap-4 flex-wrap">
+{#if projet.maquette && width > 1000}
+      <a href="{projet.maquette}" target="_blank" class="mt-6 transition text-white border rounded-md hover:border-orange hover:text-orange flex justify-center w-52"><span class="my-2 flex items-center"> Tester la maquette <External size=20/></span> </a>
+{/if}
 {#if projet.lienbouton}
-      <a href="{projet.lienbouton}" target="_blank" class="mt-6 transition text-white rounded-md bg-opacity-40 bg-orange hover:bg-opacity-100 hover:bg-white hover:text-orange flex justify-center w-40 mx-auto"><span class="my-2 flex items-center"> {projet.textebouton ? projet.textebouton : "Voir le site"}<External size=20/></span> </a>
-    {/if}
+      <a href="{projet.lienbouton}" target="_blank" class="mt-6 transition text-white rounded-md bg-opacity-40 bg-orange hover:bg-opacity-100 hover:bg-white hover:text-orange flex justify-center w-40"><span class="my-2 flex items-center"> {projet.textebouton ? projet.textebouton : "Voir le site"}<External size=20/></span> </a>
+{/if}
+</div>
+    
 </div>
 
