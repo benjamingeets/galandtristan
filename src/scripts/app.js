6f13990855d9e129193ba39f5e93934ba4e8b917
benjamingeets <benjamingeets@tutamail.com>
2022-05-12 10:20:16 +0200

    feat(services): change title on click

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,6 +13,7 @@ up.compiler('#services',()=>{
     items.forEach(item=>{
         item.addEventListener('click',()=>{
             document.querySelector('#services-text').querySelector('p').textContent = item.querySelector('p').textContent
+            document.querySelector('#services-text').querySelector('h2').textContent = item.querySelector('h3').textContent
             items.forEach(item2=>{
                 if(item2.classList.contains('bg-gradient')){
                     item2.classList.remove('bg-gradient')
