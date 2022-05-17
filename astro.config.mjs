52c92045e24c0597005adac044f6ca9a7a3560b3
benjamingeets <benjamingeets@tutamail.com>
2022-05-17 18:46:39 +0200

    feat: remove everything of svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site:'https://galandtristan.Be',
-  integrations: [svelte(), sitemap()]
+  integrations: [sitemap()]
 });
