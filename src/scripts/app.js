9fd302fc06cddfdc0cfd9f013fbdb2b99f42f0f8
benjamingeets <benjamingeets@tutamail.com>
2022-05-17 17:03:31 +0200

    feat(style): fix css not loading

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,9 @@
-import unpoly from 'unpoly'
-up.link.config.followSelectors.push('a[href]')
-up.form.config.submitSelectors.push(['form'])
-up.link.config.instantSelectors.push('a[href]')
-up.link.config.preloadSelectors.push('a[href]')
 
-up.compiler('body', () => {
+
     sendFrom()
     toggleNav()
     setSkill()
-})
+
 
 const setSkill = () => {
     const skills = document.querySelectorAll('.skill')
